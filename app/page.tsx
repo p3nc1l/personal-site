98ab023a05180bd5044b0d7cda2d27a25840340a
p3nc1l <szocsdavid598@gmail.com>
2025-08-15 21:03:08 +0300

    added subtitle to hero section

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,12 @@ import AnimatedText from "./ui/AnimatedText";
 
 const Hero = () => {
   return (
-    <div className="px-4 py-36 text-7xl font-black max-w-7xl mx-auto">
-      <div><AnimatedText textColor="oklch(62.3% 0.214 259.815)" value={["Front-end", "Back-end", "Full-stack"]} /></div>
-      <div>Web Developer</div>
+    <div className="px-4 py-36 max-w-7xl mx-auto">
+      <div className="text-7xl font-black mb-4">
+        <div><AnimatedText textColor="oklch(62.3% 0.214 259.815)" value={["Front-end", "Back-end", "Full-stack"]} /></div>
+        <div>Web Developer</div>
+      </div>
+      <div className="text-xl">Websites built on modern frameworks, with eye-catching, mobile-friendly UI.</div>
     </div>
   )
 }
