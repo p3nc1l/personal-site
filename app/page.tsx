977b2899bcbf220e01b85d1b4e648b9e91f45a52
p3nc1l <szocsdavid598@gmail.com>
2025-08-15 19:14:01 +0300

    added max-width to hero text

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,11 @@ import AnimatedText from "./ui/AnimatedText";
 
 const Hero = () => {
   return (
-  <div className="px-4 py-24 text-7xl font-black">
-    <div><AnimatedText textColor="oklch(62.3% 0.214 259.815)" value={["Front-end", "Back-end", "Full-stack"]} /></div>
-    <div>Developer</div>
-  </div>
-    )
+    <div className="px-4 py-24 text-7xl font-black max-w-7xl mx-auto">
+      <div><AnimatedText textColor="oklch(62.3% 0.214 259.815)" value={["Front-end", "Back-end", "Full-stack"]} /></div>
+      <div>Developer</div>
+    </div>
+  )
 }
 
 export default function Home() {
