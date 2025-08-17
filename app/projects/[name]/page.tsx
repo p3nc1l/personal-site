9b798ad24afc76d074b553cec79bcb77d41ac60f
p3nc1l <szocsdavid598@gmail.com>
2025-08-17 23:50:58 +0300

    added mobile responsiveness on /projects/[name]

--- app/projects/[name]/page.tsx
+++ app/projects/[name]/page.tsx
@@ -11,7 +11,7 @@ const Page = async (props: { params: Promise<{ name: string }> }) => {
   return (
     <>
       <h1 className="mt-2 mb-8 text-4xl font-bold">{project.title}</h1>
-      <div className="flex gap-4">
+      <div className="flex gap-4 flex-col sm:flex-row">
         <div className="grow-7"><ImageCarousel imageUrls={project.imageUrls} alt={project.title} /></div>
         <div className="grow-3 flex flex-col gap-4 justify-center">
           <div>{project.shortDescription}</div>
