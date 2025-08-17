d884fa10ab8fd98b523892426f18a8e84f8a20d3
p3nc1l <szocsdavid598@gmail.com>
2025-08-17 13:47:11 +0300

    removed roboto mono usage from header

--- app/ui/root-layout/HeaderNavigation.tsx
+++ app/ui/root-layout/HeaderNavigation.tsx
@@ -2,19 +2,14 @@
 
 import { usePathname } from "next/navigation";
 import Link from "next/link";
-import { Roboto_Mono } from "next/font/google";
 import { NavigationLink } from "./Header";
-
-const robotoMono = Roboto_Mono({
-  subsets: ["latin"]
-})
 
 const HeaderNavigation = (props: {links: NavigationLink[]}) => {
   const pathname = usePathname();
 
   return (
     <div className={`flex gap-6`}>
-      {props.links.map((link, index) => <Link className={ `${pathname == link.href && "font-black"} ${robotoMono.className}`} key={index} href={link.href}>{link.name}</Link>)}
+      {props.links.map((link, index) => <Link className={ `${pathname == link.href && "font-black"}`} key={index} href={link.href}>{link.name}</Link>)}
     </div>
   )
 }
--- app/ui/root-layout/HeaderSidebar.tsx
+++ app/ui/root-layout/HeaderSidebar.tsx
@@ -1,11 +1,6 @@
 import { AnimatePresence, motion } from "motion/react";
 import Link from "next/link";
 import { NavigationLink } from "./Header";
-import { Roboto_Mono } from "next/font/google";
-
-const robotoMono = Roboto_Mono({
-  subsets: ["latin"],
-})
 
 const HeaderSidebar = (props: {visible?: boolean, links: NavigationLink[], style?: string}) => {
   const visible = props.visible || false;
@@ -19,7 +14,7 @@ const HeaderSidebar = (props: {visible?: boolean, links: NavigationLink[], style
         exit={{x: "100vw"}}
         transition={{type: "tween"}}
       >
-        <div className={`flex flex-col w-full ${robotoMono.className}`}>
+        <div className={"flex flex-col w-full"}>
           {props.links.map((link, index) => 
           <Link key={index} href={link.href}>
             <div className="w-full px-4 py-4 text-2xl">{link.name}</div>
