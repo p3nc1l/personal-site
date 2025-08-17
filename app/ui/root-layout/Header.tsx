adf7f31268c3d45c04fb52056939111d363f7540
p3nc1l <szocsdavid598@gmail.com>
2025-08-17 13:53:23 +0300

    unified header components into a single one

--- app/ui/root-layout/Header.tsx
+++ app/ui/root-layout/Header.tsx
@@ -1,12 +1,11 @@
 "use client"
 
 import Link from "next/link"
-import HeaderNavigation from "./HeaderNavigation"
-import HeaderSidebarButton from "./HeaderSidebarButton"
-import HeaderSidebar from "./HeaderSidebar"
 import { useState } from "react"
+import { usePathname } from "next/navigation"
+import { motion, AnimatePresence } from "motion/react"
 
-export type NavigationLink = {
+type NavigationLink = {
   name: string,
   href: string
 }
@@ -24,6 +23,87 @@ const navigationLinks: NavigationLink[] = [{
 
 const headerStyle = "bg-white/85 text-black backdrop-blur-md";
 
+const IconPath = (props: {variant?: "menu" | "close", closeOrientation?: "left" | "right", menuHeight?: number}) => {
+  const variant = props.variant || "menu";
+  const closeOrientation = props.closeOrientation || "left";
+  const menuHeight = props.menuHeight || 0;
+
+  return (
+    <motion.line
+      className={"stroke-black"}
+      variants={{
+        menu: { x1: 0, y1: menuHeight, x2: 30, y2: menuHeight, strokeWidth: 4 },
+        close: { x1: 1, y1: closeOrientation == "right" ? 24 : 1, x2: 24, y2: closeOrientation == "right" ? 1 : 24, strokeWidth: 3 }
+      }}
+      animate={variant}
+    />
+  )
+}
+
+const HeaderSidebarButton = (props: {toggleSidebar: React.Dispatch<React.SetStateAction<boolean>>, sidebarVisible: boolean}) => {
+  const variant = props.sidebarVisible ? "close" : "menu";
+
+  const ChangeVariant = () => {
+    props.toggleSidebar(current => !current)
+  }
+
+  return (
+    <form action={ChangeVariant} className="flex">
+      <button type="submit">
+        <motion.svg 
+          width={25} 
+          height={25} 
+          className={"cursor-pointer"}
+          variants={{
+            menu: {rotate: 0},
+            close: {rotate: 90}
+          }}
+          animate={variant}
+        >
+          <IconPath variant={variant} closeOrientation="right" menuHeight={2} />
+          <IconPath variant={variant} closeOrientation="right" menuHeight={9} />
+          <IconPath variant={variant} closeOrientation="left" menuHeight={16} />
+          <IconPath variant={variant} closeOrientation="left" menuHeight={23} />
+        </motion.svg>
+      </button>
+    </form>
+  )
+}
+
+const HeaderSidebar = (props: {visible?: boolean}) => {
+  const visible = props.visible || false;
+
+  return (
+    <AnimatePresence>
+      {visible && <motion.div
+        className={`absolute w-full h-screen sm:hidden ${headerStyle}`}
+        initial={{x: "100vw"}}
+        animate={{x: 0}}
+        exit={{x: "100vw"}}
+        transition={{type: "tween"}}
+      >
+        <div className={"flex flex-col w-full"}>
+          {navigationLinks.map((link, index) => 
+          <Link key={index} href={link.href}>
+            <div className="w-full px-4 py-4 text-2xl">{link.name}</div>
+          </Link>
+          )}
+        </div>
+      </motion.div>}
+    </AnimatePresence>
+  )
+}
+
+const HeaderNavigation = () => {
+  const pathname = usePathname();
+
+  return (
+    <div className={`flex gap-6`}>
+      {navigationLinks.map((link, index) => <Link className={ `${pathname == link.href && "font-black"}`} key={index} href={link.href}>{link.name}</Link>)}
+    </div>
+  )
+}
+
 const Header = () => {
   const [sidebarVisible, setSidebarVisible] = useState(false);
 
@@ -34,12 +114,12 @@ const Header = () => {
           <Link href={"/"}>
             <span className="font-bold text-2xl">p3nc1l</span>
           </Link>
-          <span className="hidden sm:inline"><HeaderNavigation links={navigationLinks} /></span>
+          <span className="hidden sm:inline"><HeaderNavigation /></span>
           <div className="block sm:hidden"><HeaderSidebarButton sidebarVisible={sidebarVisible} toggleSidebar={setSidebarVisible} /></div>
         </div>
       </div>
       <div className="relative">
-        <HeaderSidebar visible={sidebarVisible} links={navigationLinks} style={headerStyle} />
+        <HeaderSidebar visible={sidebarVisible} />
       </div>
     </header>
   )
