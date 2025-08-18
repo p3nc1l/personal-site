2b9eaac85bcfd9178be86e4c08adcd00e349ea19
p3nc1l <szocsdavid598@gmail.com>
2025-08-18 15:46:15 +0300

    increased text size and added tags to projects page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -3,6 +3,7 @@ import projects from "./projects"
 import Card from "../ui/Card"
 import Link from "next/link"
 import Image from "next/image"
+import Tag from "../ui/Tag"
 
 export const metadata: Metadata = {
   title: "Projects",
@@ -15,15 +16,18 @@ const Page = () => {
       <h1 className="text-5xl mt-2 mb-8">Projects</h1>
       <div className="grid grid-cols-[repeat(auto-fill,minmax(200px,1fr))] gap-4">
         {projects.map((project, index) => 
-          <Card key={index}>
-            <Link href={`/projects/${project.name}`}>
+          <Link key={index} href={`/projects/${project.name}`}>
+            <Card>
               {project.imageUrls[0] ? 
               <div className={"w-full aspect-3/2 relative mb-4"}><Image className="object-cover" fill src={project.imageUrls[0]} alt={project.title} /></div> : 
               <div className={"w-full aspect-3/2 bg-black flex justify-center items-center mb-4"}>No image</div>}
-              <div className="font-bold">{project.title}</div>
-              <div className="text-sm text-justify">{project.shortDescription}</div>
-            </Link>
-          </Card>
+              <div className="font-bold text-2xl">{project.title}</div>
+              <div className="text-base text-justify">{project.shortDescription}</div>
+              <div className="flex gap-1 mt-2">
+                {project.tags.map((tag, index) => <Tag key={index}>{tag}</Tag>)}
+              </div>
+            </Card>
+          </Link>
         )}
       </div>
     </>
