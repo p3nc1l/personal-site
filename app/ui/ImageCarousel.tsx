a78445ff5d3d0b978898972fd7a7a48d76c6f388
p3nc1l <szocsdavid598@gmail.com>
2025-08-18 12:36:06 +0300

    added no image placeholder to image carousel, fixed navigation button alt text and fixed next button active if no images present

--- app/ui/ImageCarousel.tsx
+++ app/ui/ImageCarousel.tsx
@@ -7,7 +7,7 @@ import { motion } from "motion/react";
 const ImageButton = (props: {variant?: "previous" | "next", action?: () => void, disabled?: boolean}) => {
   return (
     <button className={`absolute top-[50%] -translate-y-1/2 ${props.disabled != true ? "cursor-pointer" : ""}`} disabled={props.disabled == true} onClick={props.action} style={props.variant == "previous" ? {left: "10px"} : {right: "10px"}}>
-      <div className="p-2 bg-black/30"><Image width={30} height={0} className={props.variant == "previous" ? "rotate-180" : ""} src={"/right-arrow.svg"} alt="Previous image" /></div>
+      <div className="p-2 bg-black/30"><Image width={30} height={0} className={props.variant == "previous" ? "rotate-180" : ""} src={"/right-arrow.svg"} alt={props.variant == "previous" ? "Previous image" : "Next image"} /></div>
     </button>
   )
 }
@@ -18,14 +18,19 @@ const ImageCarousel = (props: { imageUrls: string[], alt: string }) => {
   return (
     <div className="w-full relative overflow-hidden">
       <motion.div className={`flex relative`} style={{left: `${-100 * currentIndex}%`}} animate={{left: `${-100 * currentIndex}%`}}>
+        {props.imageUrls[0] == undefined && 
+          <div className="w-full aspect-video bg-neutral-700 shrink-0 relative flex items-center justify-center text-2xl">
+            <span>No image</span>
+          </div>
+        }
         {props.imageUrls.map((image, index) => 
           <div key={index} className="w-full aspect-video bg-neutral-700 shrink-0 relative">
-            <Image fill src={image} alt={`${props.alt} #${index + 1}`} />
+            <Image className="object-contain" fill src={image} alt={`${props.alt} #${index + 1}`} />
           </div>
         )}
       </motion.div>
       <ImageButton action={() => setCurrentIndex(index => index - 1)} variant="previous" disabled={currentIndex == 0} />
-      <ImageButton action={() => setCurrentIndex(index => index + 1)} variant="next" disabled={currentIndex == props.imageUrls.length - 1} />
+      <ImageButton action={() => setCurrentIndex(index => index + 1)} variant="next" disabled={currentIndex >= props.imageUrls.length - 1} />
     </div>
   )
 }
