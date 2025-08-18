799aaacfb74bc1a623b35bc87f0b9cd695c114bd
p3nc1l <szocsdavid598@gmail.com>
2025-08-18 22:21:30 +0300

    changed calorie tracker's long description

--- app/projects/projects.ts
+++ app/projects/projects.ts
@@ -13,7 +13,7 @@ const projects: Project[] = [{
   title: "Calorie Tracker",
   imageUrls: ["/calorie-tracker/home-page.png", "/calorie-tracker/foods-page-query.png", "/calorie-tracker/meal-editor.png", "/calorie-tracker/meals-page.png", "/calorie-tracker/profile-page.png"],
   shortDescription: "To keep track of your diet",
-  longDescription: "This is a simple calorie tracker app. It's abilities include looking up foods from FatSecret's food API, logging multiple foods as a meal, browsing through the logged meals, setting a daily goal, etc.",
+  longDescription: "Calorie Tracker is an app aiming to help you keep track of a healthy diet. The app is powered by a front-end and back-end service which's repositories are both publicly available on Github at <a href='https://github.com/p3nc1l/calorie-tracker'>p3nc1l/calorie-tracker</a> and <a href='https://github.com/p3nc1l/calorie-tracker-backend'>p3nc1l/calorie-tracker-backend</a>.<br><br>The features of the app include:<br><ul><li>Querying data from <a href='https://platform.fatsecret.com/'>FatSecret</a>'s food API</li><li>Logging multiple foods, whether queried or custom, as meals</li><li>Setting a daily goal</li><li>Customizing the app with nickname</li><li>Saving data to local storage</li><li>See food macros: fat, carbohydrates and protein</li></ul>",
   link: "https://calorie-tracker.p3nc1l.com",
   tags: ["React", "Vite", "Express"]
 }]
