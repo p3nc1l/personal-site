6e73262befb0df464738d50dd14025cd25bb08a8
p3nc1l <szocsdavid598@gmail.com>
2025-08-18 12:31:19 +0300

    added padding relative to the font size in Tag component

--- app/ui/Tag.tsx
+++ app/ui/Tag.tsx
@@ -6,7 +6,7 @@ import { motion } from "motion/react"
 const Tag = (props: {children: ReactNode, hoverAnimation?: boolean}) => {
   return (
     <motion.span
-      className="bg-neutral-800 border-1 border-neutral-600 w-max py-0.5 px-2 rounded-full cursor-default"
+      className="bg-neutral-800 border-1 border-neutral-600 w-max py-[0.125em] px-[0.5em] rounded-full cursor-default"
       whileHover={{scale: props.hoverAnimation ? 1.1 : 1}}
     >
       {props.children}
