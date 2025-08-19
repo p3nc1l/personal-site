d923127ffd2e6a39d63d973a3aaebffc88cd1550
p3nc1l <szocsdavid598@gmail.com>
2025-08-19 17:16:40 +0300

    added dragging to the images on /projects/[name] page

--- app/ui/ImageCarousel.tsx
+++ app/ui/ImageCarousel.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import Image from "next/image";
-import { useState } from "react";
-import { motion } from "motion/react";
+import { useState, useRef } from "react";
+import { motion, useDragControls, PanInfo } from "motion/react";
 
 const ImageButton = (props: {variant?: "previous" | "next", action?: () => void, disabled?: boolean}) => {
   return (
@@ -14,18 +14,35 @@ const ImageButton = (props: {variant?: "previous" | "next", action?: () => void,
 
 const ImageCarousel = (props: { imageUrls: string[], alt: string }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const dragControls = useDragControls();
+  const imageDivRef = useRef<HTMLDivElement>(null);
+
+  const CheckDragOffset = (info: PanInfo) => {
+    if (imageDivRef.current == undefined) return;
+    else if (info.offset.x > imageDivRef.current.getBoundingClientRect().width / 2 && currentIndex != 0) setCurrentIndex(index => index - 1);
+    else if (info.offset.x < -1 * imageDivRef.current.getBoundingClientRect().width / 2 && currentIndex != props.imageUrls.length - 1) setCurrentIndex(index => index + 1);
+  }
 
   return (
     <div className="w-full relative overflow-hidden">
-      <motion.div className={`flex relative`} style={{left: `${-100 * currentIndex}%`}} animate={{left: `${-100 * currentIndex}%`}}>
+      <motion.div 
+        className={`flex relative`} 
+        style={{left: `${-100 * currentIndex}%`}} 
+        animate={{left: `${-100 * currentIndex}%`}}
+        drag={"x"} 
+        dragControls={dragControls}
+        onDragEnd={(_, info) => CheckDragOffset(info)}
+        dragConstraints={{left: 0, right: 0}}
+        dragElastic={{left: currentIndex == props.imageUrls.length - 1 ? 0.001 : 1, right: currentIndex == 0 ? 0.001 : 1}}
+      >
         {props.imageUrls[0] == undefined && 
           <div className="w-full aspect-video bg-neutral-700 shrink-0 relative flex items-center justify-center text-2xl">
             <span>No image</span>
           </div>
         }
         {props.imageUrls.map((image, index) => 
-          <div key={index} className="w-full aspect-video bg-neutral-700 shrink-0 relative">
-            <Image className="object-contain" fill src={image} alt={`${props.alt} #${index + 1}`} />
+          <div key={index} className="w-full aspect-video bg-neutral-700 shrink-0 relative" ref={imageDivRef} >
+            <Image className="object-contain" fill src={image} alt={`${props.alt} #${index + 1}`} onPointerDown={e => dragControls.start(e)} />
           </div>
         )}
       </motion.div>
