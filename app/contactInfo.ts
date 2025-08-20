53606c69ad3008056d10575a69c2b64483d0d3db
p3nc1l <szocsdavid598@gmail.com>
2025-08-20 16:19:48 +0300

    exported contact information into separate file

---
+++ app/contactInfo.ts
@@ -0,0 +1,11 @@
+const contactInfo = [{
+  label: "Phone number",
+  href: "tel:+40770249709",
+  value: "+40 770 249 709"
+}, {
+  label: "Email address",
+  href: "mailto:david@davidszocs.com",
+  value: "david@davidszocs.com"
+}]
+
+export default contactInfo
--- app/ui/Footer.tsx
+++ app/ui/Footer.tsx
@@ -1,15 +1,8 @@
 import Link from "next/link"
+import contactInfo from "../contactInfo"
 
 const Footer = () => {
-  const contactFields = [{
-    label: "Phone number",
-    href: "tel:+40770249709",
-    value: "+40 770 249 709"
-  }, {
-    label: "Email address",
-    href: "mailto:david@davidszocs.com",
-    value: "david@davidszocs.com"
-  }]
+  const contactFields = contactInfo;
 
   const links = [{
     name: "Projects",
