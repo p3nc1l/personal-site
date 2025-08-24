8f6e672c7588c9704275a28dd00f6ec0d80a2a31
p3nc1l <szocsdavid598@gmail.com>
2025-08-24 14:40:58 +0300

    added required to the input fields on the contact page

--- app/contact/ContactForm.tsx
+++ app/contact/ContactForm.tsx
@@ -16,14 +16,14 @@ const MessageBox = (props: { data: MessageBoxData }) => {
   )
 }
 
-const InputField = (props: { name: string, type: string, label: string }) => {
+const InputField = (props: { name: string, type: string, label: string, required?: boolean }) => {
   const styleClasses = "border-neutral-400 border-1 p-2 rounded-sm bg-neutral-900 w-full";
 
   return (
     <label className="w-full">
       <div className="mb-1">{props.label}</div>
-      {props.type != "multiline" ? <input type={props.type} name={props.name} className={styleClasses} /> :
-      <textarea name={props.name} className={styleClasses + " " + "h-40"} />}
+      {props.type != "multiline" ? <input required={props.required} type={props.type} name={props.name} className={styleClasses} /> :
+      <textarea required={props.required} name={props.name} className={styleClasses + " " + "h-40"} />}
     </label>
   )
 }
@@ -34,10 +34,10 @@ const ContactForm = () => {
   return (
     <form action={formAction} className="text-xl w-full max-w-2xl flex flex-col items-center gap-2">
       {state != undefined && <MessageBox data={state} />}
-      <InputField name={"fullName"} label="Name" type="text" />
-      <InputField name={"email"} label="Email address" type="email" />
-      <InputField name={"subject"} label="Subject" type="text" />
-      <InputField name={"content"} label="Message" type="multiline" />
+      <InputField required name={"fullName"} label="Name" type="text" />
+      <InputField required name={"email"} label="Email address" type="email" />
+      <InputField required name={"subject"} label="Subject" type="text" />
+      <InputField required name={"content"} label="Message" type="multiline" />
       <div className="w-44"><button className="w-full" type="submit"><Button size="large" fullWidth loading={pending}>SUBMIT</Button></button></div>
     </form>
   )
