bc1bec0ac338ace90a497224975ca13440a33c9a
p3nc1l <szocsdavid598@gmail.com>
2025-08-24 14:56:57 +0300

    added backend data validation to contact form

--- app/contact/actions.ts
+++ app/contact/actions.ts
@@ -3,6 +3,7 @@
 import crypto from "crypto";
 import nodemailer from "nodemailer";
 import { PrismaClient } from "@/generated/prisma";
+import * as z from "zod";
 
 export type MessageBoxData = {
   error?: boolean,
@@ -78,31 +79,46 @@ export const VerifyEmail = async (token: string): Promise<boolean> => {
 
 export const SendMessage = async (formData: FormData): Promise<MessageBoxData> => {
   const token = crypto.randomBytes(32).toString("hex");
-  const prisma = new PrismaClient();
+
+  const dataSchema = z.object({
+    content: z.string(),
+    email: z.email(),
+    fullName: z.string(),
+    subject: z.string()
+  })
+
+  let submittedData;
+  
+  try {
+    submittedData = dataSchema.parse({ content: formData.get("content"), email: formData.get("email"), fullName: formData.get("fullName"), subject: formData.get("subject") });
+  }
+  catch {
+    return { message: "The data you submitted was not correct!", error: true }
+  }
 
   try {
+    const prisma = new PrismaClient();
     await prisma.message.create({
       data: {
-        content: formData.get("content") as string,
-        email: formData.get("email") as string,
-        name: formData.get("fullName") as string,
-        subject: formData.get("subject") as string,
+        content: submittedData.content,
+        email: submittedData.email,
+        name: submittedData.fullName,
+        subject: submittedData.subject,
         token: token,
         timestamp: Date.now()
       }
     });
+    prisma.$disconnect();
     await transporter.sendMail({
       from: `"No Reply - David Szocs" <${process.env.SMTP_USER}>`,
-      to: formData.get("email") as string,
+      to: submittedData.email,
       subject: "Email address verification",
       html: EmailTemplate(`${process.env.NEXT_PUBLIC_BASE_URL}/contact/${token}`),
       text: `In order for your message to be sent you need to verify the ownership of this email address. To complete the verification process, please click on the link down below:\n\n${process.env.NEXT_PUBLIC_BASE_URL}/contact/${token}\n\nIf you did not submit the contact form on my website, please ignore this email.`
     });
-    prisma.$disconnect();
-    return { message: `An email has been sent to your address to verify it's ownership. Please click on the link in the next ${messageLifetime / (1000 * 60)} minutes, before it expires.` };
   }
   catch {
-    prisma.$disconnect();
     return { message: "A system error occurred while trying to send the verification email. Please try again later!", error: true }
   }
+  return { message: `An email has been sent to your address to verify it's ownership. Please click on the link in the next ${messageLifetime / (1000 * 60)} minutes, before it expires.` };
 }
